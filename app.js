9a091334637ba27db214a9f78ba89f8ae27b1631
snak3 <fbrenomagalhaes@gmail.com>
2024-09-12 22:19:23 -0300

    Fix bugs Add Entropy Level in real-time

--- app.js
+++ app.js
@@ -86,5 +86,5 @@ app.post('/generate-seed', upload.single('fileInput'), (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
